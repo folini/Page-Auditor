88c8283e7287c0867953898750643c3cb9b57508
Dario Passariello <dariopassariello@gmail.com>
2021-12-17 17:26:15 -0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-    roots: ['<rootDir>/test'],
-    transform: {
-      '^.+\\.(ts|js)$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
-    moduleFileExtensions: ['ts', 'js', 'json'],
-    testEnvironment: 'jest-environment-jsdom',
-    setupFiles: [
-      "jsdom-worker"
-    ]
-  }
+  roots: ['<rootDir>/test'],
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(ts|tsx|js)?$": "ts-jest",
+  },
+  testRegex: "/src/.*\\.test.(ts|tsx)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testEnvironment: ["node","jest-environment-jsdom"],
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$": "identity-obj-proxy",
+  },
+  setupFiles: [
+    "./src/tests/setup-tests.ts",
+    "jsdom-worker"
+  ]
+};
