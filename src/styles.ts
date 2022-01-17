67782192e0fe2600b1a348880a3f7dc9be9bf211
Dario Passariello <dariopassariello@gmail.com>
2022-01-16 20:12:42 -0800

    new

---
+++ src/styles.ts
@@ -0,0 +1,6 @@
+
+import "Styles/less/root.less"
+import "Styles/less/fonts.less"
+import "Styles/less/icons.less"
+import "Styles/less/social.less"
+import "Styles/less/popup.less"
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
+/*
+  PLEASE DON'T CHANGE WITHOUT AUTHORIZATION
+  Franco Folini
+  Dario Passariello
+*/
+
 const path = require('path');
 const webpack = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
 const dotenv = require('dotenv').config({path: __dirname + '/.env'});
 const TerserPlugin = require("terser-webpack-plugin");
 
-let pjson = require('./package.json');
+  let pjson = require('./package.json');
+
+  module.exports = (env, argv) => {
 
   console.log(
     {
@@ -15,216 +23,216 @@ let pjson = require('./package.json');
       appName: pjson.appName,
       root: pjson.appFolder,
       folderAPI: pjson.appAPI,
+      mode: argv.mode === 'development' ? 'development' : 'production',
     }
   );
 
-module.exports = {
+  console.debug( "\n\tYour API came from: " + JSON.stringify( pjson.apiDev ) + "\n\n");
 
-  mode: 'production',
-  devtool: "hidden-nosources-source-map",
-  target: 'web',
-  cache: true,
+  return {
 
-  devServer: {
-    // port: 3000,
-    // http2: true,
-    // https: true,
-    compress: true,
-    magicHtml: false,
-    hot: false,
-    liveReload: false,
-    historyApiFallback: true,
-    contentBase: './dist',
-    open: false,
+    mode: argv.mode === 'development' ? 'development' : 'production',
+    devtool: argv.mode === 'development' ? 'eval-source-map' : 'cheap-module-source-map',
+    stats: argv.mode === 'development' ? 'errors-warnings' : '',
+    cache: argv.mode === 'development' ? false : true,
+    target: 'web',
 
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-      'Accept-Encoding': '*',
-      'Creator': 'Franco Folini (c) 2020',
-    },
+    devServer: {
 
-    client: {
-      overlay: false,
-    },
-
-  },
-
-  entry: {
-    main: './src/main.ts',
-    worker: './src/worker.ts',
-  },
-
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
-    clean: true,
-  },
-
-  performance: {
-    hints: false,
-    maxAssetSize: 500000,
-  },
-
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin(
-      {
-        terserOptions: {
-            keep_classnames: true,
-            keep_fnames: true
-        }
-      }
-    )],
-    moduleIds: 'named',
-  },
-
-  module: {
-    rules: [
-
-      // scripts loader
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        loader: require.resolve('babel-loader'),
-        exclude: [ /node_modules/, /.OLD/ ],
-        options: {
-          //plugins: ['react-hot-loader/babel'],
-          cacheDirectory: true,
-        },
+      static: {
+        directory: path.join( __dirname, 'dist'),
+        publicPath: '/',
       },
 
-      // html loader
-      {
-        test: /\.(htm)$/,
-        loader: 'file-loader',
-        options: {name: '[name].htm'},
+      port: 3000,
+      compress: argv.mode === 'development' ? false : true,
+      magicHtml: argv.mode === 'development' ? true : false,
+      liveReload: argv.mode === 'development' ? true : false,
+      hot: argv.mode === 'development' ? true : false,
+      historyApiFallback: true,
+      open: false,
+
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        'Accept-Encoding': '*',
+        'Author': 'Franco Folini (c) 2020',
       },
 
-      // less-loader
-      {
-        test: /\.(less)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "style-loader"
-          },{
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              modules: true,
-            }
-          },{
-            loader: "less-loader",
+      webSocketServer: 'ws',
+      client: {
+        overlay: true,
+        webSocketTransport: 'ws',
+        //progress: true,
+        //logging: 'info',
+      },
+
+    },
+
+    entry: {
+      main: './src/main.ts',
+      worker: './src/scripts/worker.ts',
+    },
+
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: '/'
+    },
+
+    performance: {
+      hints: argv.mode === 'development' ? 'error' : false,
+      maxEntrypointSize: 51200000,
+      maxAssetSize: 51200000
+    },
+
+    optimization: {
+      minimize: argv.mode === 'development' ? false : true,
+      minimizer: [new TerserPlugin({
+        exclude: /static/,
+      })]
+    },
+
+    resolve: {
+
+      fallback: {
+        "fs": false
+      },
+
+      alias: {
+        Assets:       path.resolve( __dirname, 'src/assets' ),
+        Include:      path.resolve( __dirname, 'src/include' ),
+        Modals:       path.resolve( __dirname, 'src/modals' ),
+        Config:       path.resolve( __dirname, 'src/config' ),
+        Data:         path.resolve( __dirname, 'src/data' ),
+        Pages:        path.resolve( __dirname, 'src/pages' ),
+        Scripts:      path.resolve( __dirname, 'src/scripts' ),
+        Styles:       path.resolve( __dirname, 'src/styles' ),
+        Layout:       path.resolve( __dirname, 'src/layout' ),
+        Root:         path.resolve( __dirname, 'src' )
+      },
+
+      modules: [path.resolve('node_modules')],
+      extensions: [".ts", ".tsx", ".js", ".less", ".css", "scss"],
+
+    },
+
+    module: {
+
+      rules: [
+
+        // we use babel-loader to load our jsx and tsx files
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          loader: require.resolve('babel-loader'),
+          exclude: [ /node_modules/, /.OLD/ ],
+          options: {
+            //plugins: ['react-hot-loader/babel'],
+            cacheDirectory: argv.mode === 'development' ? true : false,
           },
-        ],
-      },
+        },
 
-      // scss / css loader
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: true,
-              importLoaders: 1,
+        // html loader
+        {
+          test: /\.(htm)$/,
+          loader: 'file-loader',
+          options: {name: '[name].htm'},
+        },
+
+        // css-loader and less-loader
+        {
+          test: /\.(less)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: "style-loader"
+            },{
+              loader: "css-loader",
+              options: {
+                url: false,
+                modules: false,
+                sourceMap: false,
+                importLoaders: 1,
+              }
+            },{
+              loader: "less-loader"
             },
-          },
-          'sass-loader',
-        ],
-        include: /\.module\.s?(c|a)ss$/,
-      },
+          ],
+        },
 
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        exclude: /\.module\.s?(c|a)ss$/,
-      },
-
-      // All assets
-      {
-        test: /\.(woff(2)?|ttf|eot|svg|png|jpe?g|gif)(\?v=\d+\.\d+\.\d+)?$/,
-        //type: 'asset/resource',
-        loader: 'file-loader',
-        //options: {name: '[name].[ext]'},
-      },
-
-      // {
-      //   test: /manifest\.(json)$/,
-      //   loader: 'file-loader',
-      //   options: {name: '[name].json'},
-      //   type: 'javascript/auto',
-      // },
-
-      // {
-      //   test: [/Logo_256x256\.(png)$/, /_noRendering_400x200\.(png)$/, /fa-regular-400\.(woff)$/],
-      //   loader: 'file-loader',
-      //   options: {
-      //     name: '[name].[ext]',
-      //     outputPath: 'logos',
-      //   },
-      // },
-
-      // {
-      //   test: /\.(less)$/,
-      //   use: ['style-loader', 'css-loader', 'less-loader'],
-      // },
-
-    ],
-  },
-
-  resolve: {
-    alias: {
-      Assets:       path.resolve( __dirname, 'src/assets' ),
-      Components:   path.resolve( __dirname, 'src/components' ),
-      Include:      path.resolve( __dirname, 'src/shared/include' ),
-      Modals:       path.resolve( __dirname, 'src/shared/modals' ),
-      Reports:      path.resolve( __dirname, 'src/shared/reports' ),
-      Shared:       path.resolve( __dirname, 'src/shared' ),
-      App:          path.resolve( __dirname, 'src/app' ),
-      Config:       path.resolve( __dirname, 'src/config' ),
-      Data:         path.resolve( __dirname, 'src/data' ),
-      Pages:        path.resolve( __dirname, 'src/pages' ),
-      Scripts:      path.resolve( __dirname, 'src/scripts' ),
-      Styles:       path.resolve( __dirname, 'src/styles' ),
-      Typings:      path.resolve( __dirname, 'src/typings' ),
-      Utils:        path.resolve( __dirname, 'src/utils' ),
-      Validators:   path.resolve( __dirname, 'src/validators' ),
-      Layout:       path.resolve( __dirname, 'src/layout' ),
-      Root:         path.resolve( __dirname, 'src/' )
-    },
-    modules: [path.resolve('node_modules')],
-    extensions: [".ts", ".tsx", ".js", ".css", ".less", ".sass", ".scss"],
-  },
-
-  plugins: [
-
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-      'process.env': dotenv.parsed,
-      'process.env.debug': false
-    }),
-
-    new CopyPlugin({
-      patterns: [
+        // CSS, SCSS
         {
-          from: "./src/assets/",
-          to: "./assets/",
-          // globOptions: {
-          //   ignore: ['**/documents']
-          // }
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+                modules: false,
+                sourceMap: false,
+                importLoaders: 1,
+              },
+            },
+            'sass-loader',
+          ],
+          include: /\.module\.s?(c|a)ss$/,
         },
         {
-          from: "./src/manifest.json",
-          to: "./manifest.json",
-        }
-      ],
-    }),
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+          exclude: /\.module\.s?(c|a)ss$/,
+        },
 
-  ].filter(Boolean)
+          // assets
+          {
+            test: /\.(a?png|jpe?g|gif|ico|bmp|webb)(\?v=\d+\.\d+\.\d+)?$/,
+            type: 'asset/resource',
+            exclude: [ /node_modules/, /.OLD/ ],
+          },
+
+          // fonts
+          {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            type: 'asset/resource',
+            exclude: [ /node_modules/, /.OLD/ ],
+          }
+
+      ],
+    },
+
+    plugins: [
+
+      //Environment
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify( argv.mode ),
+        'process.env': dotenv.parsed,
+        'process.env.debug': argv.mode === 'development' ? true : false
+      }),
+
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "./src/assets/",
+            to: "./assets/",
+            globOptions: {
+              gitignore: true,
+            }
+          },
+          {
+            from: "./src/manifest.json",
+            to: "./manifest.json",
+            globOptions: {
+              gitignore: true,
+            }
+          }
+        ],
+      }),
+
+    ].filter(Boolean)
+
+  };
 
 };
